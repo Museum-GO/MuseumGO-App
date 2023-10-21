f73ad9673595ca702304197c8888d41fd9b73100
tom.mansion <tomansion@yahoo.fr>
2023-10-21 14:24:14 +0200

    Added router, vuex and i18n

--- front/README.md
+++ front/README.md
@@ -22,3 +22,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Recommended extensions
+
+- cspell: Spell checker
+- .18n Ally: i18n support
--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="testCompo">
+    <p>{{ $t("hello") }}</p>
+    <p>{{ variable }}</p>
+
+    <input type="number" v-model="newVariable" />
+    <button @click="setVariable">Change</button>
+    <div id="message">
+      <p v-if="isVariableCorrect">{{ $t("variableCorrect") }}</p>
+      <p v-else class="error">{{ $t("variableIncorrect") }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  data() {
+    return {
+      variable: 10,
+      newVariable: 20,
+    };
+  },
+  created() {
+    // Called when the component is created.
+  },
+  mounted() {
+    // Called when the component has been mounted in the DOM.
+  },
+  methods: {
+    // Any methods you want to use in your component.
+    setVariable() {
+      this.variable = parseInt(this.newVariable);
+    },
+  },
+  computed: {
+    // Any computed properties you want to use in your component.
+    isVariableCorrect() {
+      return this.variable >= 0;
+    },
+  },
+  watch: {
+    // Any watchers you want to use in your component.
+    variable(newValue, oldValue) {
+      console.log(`Variable changed from ${oldValue} to ${newValue}`);
+    },
+  },
+
+  // The following hooks won't be very useful in most cases.
+  beforeUpdate() {
+    // Called when the component is about to update.
+    console.log("beforeUpdate");
+  },
+
+  updated() {
+    // Called after a data change causes the virtual DOM to be re-rendered and patched.
+    console.log("updated");
+  },
+
+  beforeUnmount() {
+    // Called when the component is about to be unmounted.
+  },
+
+  unmounted() {
+    // Called when the component has been removed from the DOM.
+  },
+});
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+// Scoped styles
+#testCompo {
+  color: red;
+
+  #message {
+    color: green;
+    .error {
+      color: red;
+    }
+  }
 }
 </style>
