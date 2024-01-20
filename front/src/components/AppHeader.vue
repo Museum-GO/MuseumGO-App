436d03e0f5dfe81a91c007f247a780854e4b25da
Smartycrouton <59928333+Smartycrouton@users.noreply.github.com>
2024-01-20 16:32:04 +0100

    Update appheader
    Add shortcut to the main

--- front/src/components/AppHeader.vue
+++ front/src/components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <header id="header">
 
-    <div class="app-name" > <a href="#main"> MuseumGo</a> </div>
-        
-        <div class="search-bar">
-            <input type="text" placeholder="Recherche">
-        </div>
+    <div class="app-name"> <a href="/">MuseumGo</a> </div>
 
-        <div class="language-link"> <a href="?fr">FR</a> | <a href="?eng">ENG</a> </div>
+    <div class="search-bar">
+      <input type="text" placeholder="Recherche">
+    </div>
+
+    <div class="language-link"> <a href="?fr">FR</a> | <a href="?eng">ENG</a> </div>
   </header>
 </template>
 
@@ -17,43 +17,43 @@ export default {
 };
 </script>
 
-<style>
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
+<style lang="scss" scoped>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
 
-        #header {
-            background-color: #ffffff;
-            padding: 10px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+#header {
+  background-color: #ffffff;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 
-        .app-name {
-            font-size: 24px;
-            font-weight: bold;
-        }
+.app-name {
+  font-size: 24px;
+  font-weight: bold;
 
-        .app-name a{
-          color: #333333;
-          text-decoration: none;
-        }
+  a {
+    color: #333333;
+    text-decoration: none;
+  }
+}
 
-        .search-bar {
-            margin-left: 20px;
-        }
+.search-bar {
+  margin-left: 20px;
+}
 
-        .language-link {
-            color: #333333;
-            font-weight: bold;
-            margin-right: 20px;
-        }
+.language-link {
+  color: #333333;
+  font-weight: bold;
+  margin-right: 20px;
 
-        .language-link a{
-          color: #333333;
-          text-decoration: none;
-        }
-    </style>
+  a {
+    color: #333333;
+    text-decoration: none;
+  }
+}
+</style>
