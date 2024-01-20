6a61eb237dd2a700767583b733d4d6177be47549
Smartycrouton <59928333+Smartycrouton@users.noreply.github.com>
2024-01-21 00:33:40 +0100

    Header - languages options
    Task 5 finish

--- front/src/components/AppHeader.vue
+++ front/src/components/AppHeader.vue
@@ -1,3 +1,5 @@
+<!-- TEMPLATE ---------------------------------------------------------------------------------------------------------------------------------->
+
 <template>
   <header id="header" lang="fr">
 
@@ -7,46 +9,55 @@
       <input type="text" :placeholder='$t("Header.search-bar")'>
     </div>
 
-    <div class="language-link"> <a href="?fr">{{$t("Header.fr") }}</a> | <a href="?eng">{{$t("Header.eng") }}</a> </div>
+    <div class="language-link">  
+      <select v-model="$i18n.locale">
+        <option value="fr">fr</option>
+        <option value="en">en</option>
+      </select>
+</div>
   </header>
 </template>
 
+<!-- SCRIPT ---------------------------------------------------------------------------------------------------------------------------------->
+
 <script>
-export default {
-  name: "AppHeader",
-};
+  export default {
+    name: "AppHeader",
+  };
 </script>
 
+<!-- CSS ---------------------------------------------------------------------------------------------------------------------------------->
+
 <style lang="scss" scoped>
-body {
-  margin: 0;
-  font-family: Arial, sans-serif;
-}
+  body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+  }
 
-#header {
-  background-color: #333333;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
+  #header {
+    background-color: #333333;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
 
-a {
-    color: #d3d3d3;
-    text-decoration: none;
-    font-weight: bold;
-}
+  a {
+      color: #d3d3d3;
+      text-decoration: none;
+      font-weight: bold;
+  }
 
-.app-name {
-  font-size: 24px;
-}
+  .app-name {
+    font-size: 24px;
+  }
 
-.search-bar {
-  margin-left: 20px;
-}
+  .search-bar {
+    margin-left: 20px;
+  }
 
-.language-link {
-  margin-right: 20px;
-}
+  .language-link {
+    margin-right: 20px;
+  }
 </style>
