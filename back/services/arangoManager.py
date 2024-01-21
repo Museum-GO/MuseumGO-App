63cc8113f8883b4c653e43c2849aed560404774e
alame <alame@localhost.localdomain>
2024-01-21 02:11:58 +0100

    Additional webscrabing base on the Louvre database

--- back/services/arangoManager.py
+++ back/services/arangoManager.py
@@ -35,23 +35,23 @@ def setup():
 
     try:
         # Delete the collections if it exist
-        db.delete_collection(WORKS_COLLECTION_NAME)
-
-        # Insert few documents into the collection for testing purposes
-        add_work("Mona Lisa", [2.335, 48.861])
-        add_work("The Starry Night", [4.833, 52.367])
-        add_work("The Last Supper", [9.19, 45.464])
-        add_work("The Creation of Adam", [12.483, 41.898])
-        add_work("The Persistence of Memory", [-73.962, 40.781])
-        add_work("The Scream", [10.738, 59.913])
-        add_work("Guernica", [-2.988, 43.319])
-
+        #db.delete_collection(WORKS_COLLECTION_NAME)
         # Create the collections if they don't exist
         if not db.has_collection(WORKS_COLLECTION_NAME):
             print(
                 f" - Creating collection {colored(WORKS_COLLECTION_NAME, DEBUG_COLOR)}"
             )
             db.create_collection(WORKS_COLLECTION_NAME)
+
+        # Insert few documents into the collection for testing purposes
+        add_work({"name": "Autoportrait à la palette","image": "https://collections.louvre.fr/media/cache/small/0000000021/0000054940/0000797482_OG.JPG","description": {"fr": "Œuvre récupérée à la fin de la Seconde Guerre mondiale, déposée par l'office des biens et intérêts privés (OBIP); en attente de sa restitution à ses légitimes propriétaires. Consulter la base de données ministérielle Rose Valland consacrée aux œuvres dites MNR (Musées nationaux récupération). "},"location": {"type": "Point","coordinates": [48.860611, 2.337644],"name": "musee du louvre"},"artists": ["François-André Vincent"],"style": {"id": 1,"name": {"fr": "Peinture","en": "Painting"}},"minDate": 1769 ,"maxDate": 1770})
+        add_work({"name": "Paysage avec chute d'eau","image": "https://collections.louvre.fr/media/cache/small/0000000021/0000055427/0000767624_OG.JPG","description": {"fr": " Pendant de INV 3220. Ancienne collection de Louis XVI, provient de Fontainebleau. Dépôt à Maisons-Laffitte en 1912 (?); retour au musée du Louvre le 18 avril 1944; dépôt au ministère des Affaires étrangères le 9 novembre 1944; retour au Louvre le 15 novembre 1944; dépôt à Maisons-Laffitte en 1948; retour au Louvre le 18 octobre 1989; dépôt à l'Hôtel Marigny le 4 décembre 1997; retour au musée du Louvre 14 décembre 2017"},"location": {"type": "Point","coordinates": [48.860611, 2.337644],"name": "musee du louvre"},"artists": ["Claude Louis Chatelet"],"style": {"id": 1,"name": {"fr": "Peinture","en": "Painting"}},"minDate": 1781})
+        add_work({"name": "Le Christ en croix","image": "https://collections.louvre.fr/media/cache/small/0000000021/0000056784/0001136914_OG.JPG","description": {"fr": "Pas de description"},"location": {"type": "Point","coordinates": [48.8526049229,2.33466199468],"name": "musee eugene delacroix"},"artists": ["Eugene Delacroix"],"style": {"id": 1,"name": {"fr": "Peinture","en": "Painting"}},"minDate": 1800,"maxDate": 1900})
+        add_work({"name": "Mort de Sardanapale","image": "https://collections.louvre.fr/media/cache/small/0000000021/0000059166/0001136918_OG.JPG","description": {"fr": "Peint en 1844, copie de la répétion réduite exécutée par Delacroix en 1844 (coll. McIlhenny, Philadelphie) de sa composition du Salon de 1827 (Louvre, R.F. 2346); acheté à Paris, à l'hôtel Drouot (étude Ader), en 1962 (vente non identifiée) "},"location": {"type": "Point","coordinates": [48.8526049229,2.33466199468],"name": "musee eugene delacroix"},"artists": ["Frédéric Villot"],"style": {"id": 1,"name": {"fr": "Peinture","en": "Painting"}},"minDate": 1844})
+        add_work({"name": "PA_177","image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Space_Invader_-_Rue_du_29_Juillet%2C_Paris_1_November_2013.jpg/220px-Space_Invader_-_Rue_du_29_Juillet%2C_Paris_1_November_2013.jpg","description": {"fr": "Oeuvre réalisé en 1999, elle représente une créature du jeu space invader"},"location": {"type": "Point","coordinates": [48.865486327298036, 2.330770955848266],"name": "Space Invader"},"artists": ["Invader"],"style": {"id": 2,"name": {"fr": "Mosaique","en": "Mosaic"}},"minDate": 1999})
+
+
+
 
     except exceptions.CollectionListError as e:
         print(colored(" - Error while creating collections", ERROR_COLOR))
--- scripts/script.py
+++ scripts/script.py
@@ -1,11 +1,47 @@
-import csv
-model={"name": "TO CHANGE","image": "TO CHANGE","description": {"fr": "To Change"},"location": {"type": "Point","coordinates": [48.860611, 2.337644],"name": "musee du louvre"},"artists": [],"style": {"id": 1,"name": {"fr": "Peinture","en": "Painting"}},"minDate": 0,"maxDate": 0}
-with open('Louvre.csv', newline='') as csvfile:
+import csv, copy,requests
+from bs4 import BeautifulSoup
+model={"name": "TO CHANGE","image": "TO CHANGE","description": {"fr": "To Change"},"location": {"type": "Point","coordinates": [48.860611, 2.337644],"name": "musee du louvre"},"artists": [],"style": {"id": 1,"name": {"fr": "Peinture","en": "Painting"}},"minDate": 0}
+model_list=[]
+with open('Louvre-extract.csv', newline='') as csvfile:
     
     reader = csv.reader(csvfile,delimiter=";")
     print(type(reader))
     #We skip the header to get our data directly
     next(reader,None)
     for row in reader:
-        print(row)
-    print(type(reader))
+    #We analys the current art work
+        base_url="https://collections.louvre.fr/ark:/53355/"
+        current_model=copy.deepcopy(model)
+        current_model["name"]=row[1]
+        #Decompose the artist name the ; is to remove ; France
+        artist_name=row[2].split(";")[0].split(",")
+        artist_toadd=artist_name[0]
+        if(len(artist_name)>1):
+            artist_toadd=artist_name[1]+" "+artist_toadd
+
+        current_model["artists"].append(artist_toadd)
+        date=row[3].split("/")
+        current_model["minDate"]=date[0].replace(' ','')
+        if(len(date)>1):
+            current_model["maxDate"]=(date[1])[0:5].replace(' ','')
+        # this part webscrap the Louvre website
+        page=requests.get(base_url+row[0])
+        soup = BeautifulSoup(page.content, "html.parser")
+        result_dom=soup.find_all("picture")
+        for current_class in result_dom:
+            pic_parameter=current_class.find("img")
+            if(pic_parameter.get("src",False)):
+                current_model["image"]="https://collections.louvre.fr/"+pic_parameter["src"]
+        #We should get the description parameter
+        history_entries=soup.find_all("div",class_="notice__mnr")
+        print("ENTRIES")
+        for entry in history_entries:
+            result=entry.find("span",class_="lbl_10")
+            print(result.text)
+
+
+
+        
+
+        #print(row)
+        
