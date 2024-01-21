8316199728096d8fe72a3a7f660b7fd0502a736c
Smartycrouton <59928333+Smartycrouton@users.noreply.github.com>
2024-01-21 02:28:55 +0100

    Prettier appheader

--- front/src/components/AppHeader.vue
+++ front/src/components/AppHeader.vue
@@ -1,64 +1,59 @@
-<!-- TEMPLATE ---------------------------------------------------------------------------------------------------------------------------------->
-
 <template>
   <header id="header" lang="fr">
-
-    <div class="app-name"> <a href="/">{{$t("Header.app-name") }}</a> </div>
-
-    <div class="search-bar">
-      <input type="text" :placeholder='$t("Header.search-bar")'>
+    <div class="app-name">
+      <a href="/">{{ $t("Header.app-name") }}</a>
     </div>
 
-    <div class="language-link">  
+    <div class="search-bar">
+      <input type="text" :placeholder="$t('Header.search-bar')">
+    </div>
+
+    <div class="language-link">
       <select v-model="$i18n.locale">
         <option value="fr">fr</option>
         <option value="en">en</option>
       </select>
-</div>
+    </div>
   </header>
 </template>
 
-<!-- SCRIPT ---------------------------------------------------------------------------------------------------------------------------------->
-
 <script>
-  export default {
-    name: "AppHeader",
-  };
+export default {
+  name: "AppHeader",
+};
 </script>
 
-<!-- CSS ---------------------------------------------------------------------------------------------------------------------------------->
-
 <style lang="scss" scoped>
-  body {
-    margin: 0;
-    font-family: Arial, sans-serif;
-  }
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
 
-  #header {
-    background-color: #333333;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
+#header {
+  background-color: #333333;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 
-  a {
-      color: #d3d3d3;
-      text-decoration: none;
-      font-weight: bold;
-  }
+a {
+  color: #d3d3d3;
+  text-decoration: none;
+  font-weight: bold;
+}
 
-  .app-name {
-    font-size: 24px;
-    flex-grow: 1;
-  }
+.app-name {
+  font-size: 24px;
+  flex-grow: 1;
+}
 
-  .search-bar {
-    margin-left: 10px; /* Ajout d'une marge entre la barre de recherche et l'élément précédent */
-    margin-right: 10px; /* Ajout d'une marge entre la barre de recherche et l'élément suivant */
-  }
+.search-bar {
+  margin-left: 10px;
+  margin-right: 10px;
+}
 
-  .language-link {
-    margin-right: 20px;
-  }
+.language-link {
+  margin-right: 20px;
+}
 </style>
